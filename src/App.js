824f1868d8744655ae72052692b81796c8906ed4
masaya48 <msya0408@gmail.com>
2017-01-27 18:45:06 +0900

    first commit!!!

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import MainSection from './component/MainSection'
+
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="App">
+        <MainSection />
+			</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/container/President.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PresidentSection from '../component/PresidentSection'
+
+class President extends Component {
+  render() {
+    return (
+
+      <div className="">
+        <PresidentSection />
+	  </div>
+    );
+  }
+}
+
+export default President;
